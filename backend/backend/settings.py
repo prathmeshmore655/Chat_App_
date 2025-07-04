4822b1933bf1c997bd830c48a5ac15ac5435b4fe
Prathmesh More <156777014+prathmeshmore655@users.noreply.github.com>
2025-07-04 13:26:52 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,8 +78,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
-            # "hosts": [("localhost", 6379)],
+            #"hosts": [("redis", 6379)],
+            "hosts": [("localhost", 6379)],
         },
     },
 }
